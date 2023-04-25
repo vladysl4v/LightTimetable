d1dd1ba13f2b4bb5d5af25b31cd16a20e78f959f
vladysl4v <t1ton7@yahoo.com>
2023-04-25 23:49:16 +0300

    Replace old input box

---
+++ LightTimetable/Tools/UtilityWindows/InputBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace LightTimetable.Tools.UtilityWindows
+{
+    public partial class InputBox
+    {
+        private bool _isOkClicked;
+        private InputBox(string title, string bodyText, string defaultText)
+        {
+            InitializeComponent();
+            Title = title;
+            BodyLabel.Text = bodyText;
+            InputField.Text = defaultText;
+        }
+
+        public static string Show(string title, string bodyText, string defaultText = "")
+        {
+            var inputWindow = new InputBox(title, bodyText, defaultText);
+            inputWindow.ShowDialog();
+            return inputWindow.InputField.Text;
+        }
+
+        private void OnWindowClosing(object? s, EventArgs e)
+        {
+            if (!_isOkClicked)
+                InputField.Text = string.Empty;
+        }
+        private void OKButton_Clicked(object s, EventArgs e)
+        {
+            _isOkClicked = true;
+            Close();
+        }
+        private void CloseButton_Clicked(object s, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
